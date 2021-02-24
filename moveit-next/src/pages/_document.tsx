2b052f96b386298260195b3e06412e41a49886c8
Thays <thayscasado@hotmail.com>
2021-02-23 22:23:20 -0800

    Component Countdown timer

--- moveit-next/src/pages/_document.tsx
+++ moveit-next/src/pages/_document.tsx
@@ -1,5 +1,4 @@
 /* just load once when the page is opened*/
-
 import Document, { Html, Head, Main, NextScript} from 'next/document'
 
 export default class MyDocument extends Document {
@@ -7,6 +6,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shotcut icon" href="favicon.png" type="imagem/png"/>
+
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"/>
         </Head>
