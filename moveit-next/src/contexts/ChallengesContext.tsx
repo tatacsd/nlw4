01109547515f52cdd0fb9323972537660348a052
Thays <thayscasado@hotmail.com>
2021-02-24 22:15:13 -0800

    Added function challenge

--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,11 +1,21 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json'
+
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+
+}
 
 interface ChallengeContextData {
   level: number; 
   currentExperience: number; 
   challengesCompleted: number; 
   levelUp: () => void;
-  startNewChallange: () => void;  
+  startNewChallange: () => void; 
+  activeChallenge: Challenge;
 }
 
 interface ChallengesProviderProps {
@@ -18,14 +28,22 @@ export const ChallengesContext = createContext({} as ChallengeContextData);
 export function ChallengesProvider({ children}: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
   const [currentExperience, setCurrentExperience] = useState (0);
-  const[challengesCompleted, setChallengesCompleted] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  const [activeChallenge, setActiveChallenge] = useState(null);
 
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallange(){
-    console.log('New Challenge')
+    // Pick a random challenge
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+
+
   }
 
 
@@ -36,7 +54,8 @@ export function ChallengesProvider({ children}: ChallengesProviderProps) {
         currentExperience, 
         challengesCompleted, 
         levelUp, 
-        startNewChallange
+        startNewChallange,
+        activeChallenge
       }}
     >
       {children}
