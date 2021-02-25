1a741da5598fcbc36a7660d749fa2188083651cf
Thays <thayscasado@hotmail.com>
2021-02-24 22:32:31 -0800

    Added percentage bar formula and level modification

--- moveit-next/src/components/ExperienceBar.tsx
+++ moveit-next/src/components/ExperienceBar.tsx
@@ -8,15 +8,16 @@ export function ExperienceBar(){
   // get user experience information 
   const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
 
+  const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
 
   return(
     <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
         {/* it is inline because the css file is for unmutable things. */}
-        <div style={{ width: '50%' }} />
+        <div style={{ width: `${percentToNextLevel}` }} />
 
-        <span className= { styles.currentExperience} style={{ left: '50%' }}>
+        <span className= { styles.currentExperience} style={{ left: `${percentToNextLevel}` }}>
           {currentExperience} xp
         </span>
       </div>
