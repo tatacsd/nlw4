1d21cd179ba728d4961a994c4efe84011461b521
Thays <thayscasado@hotmail.com>
2021-02-25 18:13:55 -0800

    Context blueprint

---
+++ moveit-next/src/contexts/CountdownContext.tsx
@@ -0,0 +1,21 @@
+import { Children, createContext, ReactNode } from "react";
+
+interface CountdownContextData {
+
+}
+
+interface CountdownProviderProps {
+  children: ReactNode;
+}
+
+const CountdownContext = createContext ({} as CountdownContextData)
+
+export function CountdownProvider({ children }: CountdownProviderProps) {
+
+  
+  return(
+    <CountdownContext.Provider value={{ }}>
+      {Children}
+    </CountdownContext.Provider>
+  );
+}
