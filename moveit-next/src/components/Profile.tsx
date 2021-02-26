7f37aa5319c1c8a90b796cd1658478bf541b558e
Thays <thayscasado@hotmail.com>
2021-02-25 18:14:16 -0800

    Profile add level up to

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile(){
+
+  const { level } = useContext(ChallengesContext);
+
   return(
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/29147847?s=460&u=14c41901b4abe559dab7c4c7ecb7898474f266a4&v=4" alt="Thays Casado"/>
@@ -8,7 +13,7 @@ export function Profile(){
         <strong>Thays Casado</strong>
         <p>
           <img src="icons/level.svg" alt="Level arrow"/>
-          Level 1
+          Level { level }
         </p>
       </div>
     </div>
